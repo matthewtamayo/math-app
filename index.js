715f93b669ac9bda12660b9df6722921909f6cef
Matthew Aaron Clifford Tamayo <matthewaaronclifford.tamayo@telus.com>
2022-06-06 21:57:33 +0800

    style: add space to outputs

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const addNumber = (a, b) => {
     return a + b
 }
 
-console.log("The sum of 7 and 4 is" + addNumber(7, 4))
+console.log("The sum of 7 and 4 is " + addNumber(7, 4))
 
 const subtractNumber = (a, b) => {
 
     return a - b
 }
 
-console.log("The different of 4 and 7 is" + subtractNumber(4, 7))
+console.log("The different of 4 and 7 is " + subtractNumber(4, 7))
 
 module.exports = {
     addNumber, subtractNumber
