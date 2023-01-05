03608f159cd7b737e16a6f17b754bfcc057ab65a
Matthew Aaron Clifford Tamayo <matthewaaronclifford.tamayo@telus.com>
2023-01-06 03:38:08 +0800

    feat: update commits

--- extract-data.sh
+++ extract-data.sh
@@ -2,6 +2,8 @@
 
 echo "sha" $GITHUB_SHA
 echo "tags"
-PREVIOUS_TAG_COMMIT=$(git rev-list -n 1 $(git describe --abbrev=0)^)
+LATEST_TAG=$(git describe --abbrev=0)
+PREV_TAG=$(git describe --abbrev=0 $LATEST_TAG^)
+PREVIOUS_TAG_COMMIT=$(git rev-list -n 1 $PREV_TAG)
 echo $PREVIOUS_TAG_COMMIT
 git rev-list $GITHUB_SHA ^$PREVIOUS_TAG_COMMIT --timestamp
