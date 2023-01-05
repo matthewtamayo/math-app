afa2a827e2a84481b95ce3ef5cffd1faffae2c99
Matthew Aaron Clifford Tamayo <matthewaaronclifford.tamayo@telus.com>
2023-01-06 03:17:17 +0800

    fix: fix previous tag commit command

--- extract-data.sh
+++ extract-data.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 echo "sha" $GITHUB_SHA
-PREV_TAG=$(git describe --tags)
-echo "prev tags" $PREV_TAG
 echo "tags"
-PREVIOUS_TAG_COMMIT=$(git rev-list -n 1 $PREV_TAG^)
+PREVIOUS_TAG_COMMIT=$(git rev-list -n 1 $(git describe --abbrev=0)^)
 echo $PREVIOUS_TAG_COMMIT
 git rev-list $GITHUB_SHA ^$PREVIOUS_TAG_COMMIT --timestamp
