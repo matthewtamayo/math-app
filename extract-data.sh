fc3ff3570b2b3656cd8916824fc66e2c5cb321b2
Matthew Aaron Clifford Tamayo <matthewaaronclifford.tamayo@telus.com>
2023-01-06 03:11:32 +0800

    feat: print prev tags

--- extract-data.sh
+++ extract-data.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo "sha" $GITHUB_SHA
+PREV_TAG=$(git describe --tags)
+echo "prev tags" $PREV_TAG
 echo "tags"
-PREVIOUS_TAG_COMMIT=$(git rev-list -n 1 $(git describe --tags)^)
+PREVIOUS_TAG_COMMIT=$(git rev-list -n 1 $PREV_TAG^)
 echo $PREVIOUS_TAG_COMMIT
 git rev-list $GITHUB_SHA ^$PREVIOUS_TAG_COMMIT --timestamp
