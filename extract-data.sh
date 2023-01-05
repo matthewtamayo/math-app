3cfd04814d7cb9ab62bf23e4fe678619012f57dc
Matthew Aaron Clifford Tamayo <matthewaaronclifford.tamayo@telus.com>
2023-01-06 03:06:18 +0800

    feat: add previous tag commit

--- extract-data.sh
+++ extract-data.sh
@@ -3,4 +3,5 @@
 echo "sha" $GITHUB_SHA
 echo "tags"
 PREVIOUS_TAG_COMMIT=$(git rev-list -n 1 $(git describe --tags)^)
+echo $PREVIOUS_TAG_COMMIT
 git rev-list $GITHUB_SHA ^$PREVIOUS_TAG_COMMIT --timestamp
