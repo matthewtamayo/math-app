c1593e32ad19b9e65fb71ea88c3c1274b66d9ea8
Matthew Aaron Clifford Tamayo <matthewaaronclifford.tamayo@telus.com>
2023-01-06 03:43:31 +0800

    feat: add identifiers

--- extract-data.sh
+++ extract-data.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 echo "sha" $GITHUB_SHA
-echo "tags"
 LATEST_TAG=$(git describe --abbrev=0)
+echo "LATEST TAG="$LATEST_TAG
 PREV_TAG=$(git describe --abbrev=0 $LATEST_TAG^)
+echo "PREV TAG="$PREV_TAG
 PREVIOUS_TAG_COMMIT=$(git rev-list -n 1 $PREV_TAG)
-echo $PREVIOUS_TAG_COMMIT
+echo "PREVIOUS TAG COMMIT="$PREVIOUS_TAG_COMMIT
+echo "list of commits"
 git rev-list $GITHUB_SHA ^$PREVIOUS_TAG_COMMIT --timestamp
